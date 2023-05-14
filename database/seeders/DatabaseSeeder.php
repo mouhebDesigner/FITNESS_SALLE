0bbce4764979c98247caab9fd31534000d25c43a
mouhebDesigner <mouhebabderrahim@gmail.com>
2023-05-15 01:22:42 +0200

    SOME

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use DB;
 use App\Models\RandomNumber;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,7 +16,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(4)->create();
+        DB::table('users')->insert(
+            [
+                [
+                    "nom" => "admin",
+                    "prenom" => "admin",
+                    "email" => "admin@gmail.com",
+                    "numTel" => 87445566,
+                    "cin" => 12345656   ,
+                    "role" => json_encode(['ROLE_ADMIN' => true]),
+                    "password" => Hash::make("adminadmin"),
+                    "date_naissance" => "1983-03-06 15:18:39"
+                ],
+                [
+                    "nom" => "client",
+                    "prenom" => "client",
+                    "email" => "client@gmail.com",
+                    "numTel" => 87445556,
+                    "cin" => 12345646   ,
+                    "role" => json_encode(['ROLE_CLIENT' => true]),
+                    "password" => Hash::make("adminadmin"),
+                    "date_naissance" => "1983-03-06 15:18:39"
+                ]
+            ]
+        );
+        // \App\Models\User::factory(4)->create();
         \App\Models\Category::factory(5)->create();
         \App\Models\Salle::factory(5)->create();
         \App\Models\Activity::factory(4)->create();
